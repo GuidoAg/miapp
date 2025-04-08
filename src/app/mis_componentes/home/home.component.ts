43b900aabf8606102826b886aac9d69edeb6ee9b
GuidoInsua <insua.guido@gmail.com>
2025-04-08 01:06:52 -0300

    suma y promedio

--- src/app/mis_componentes/home/home.component.ts
+++ src/app/mis_componentes/home/home.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  edadUno: number = 0;
+  edadDos: number = 0;
+  suma: number = 0;
+  promedio: number = 0;
 
+  calcular() {
+    this.suma = this.edadUno + this.edadDos;
+    this.promedio = this.suma / 2;
+  }
+
+  limpiar() {
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.suma = 0;
+    this.promedio = 0;
+  }
 }
